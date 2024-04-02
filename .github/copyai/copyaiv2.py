364c504d7da92c8bee8d65f35040160c291015ef
Dennizz <root@ipaddr.nl>
2024-04-02 12:08:19 +0200

    added print statement and exit program.

--- .github/copyai/copyaiv2.py
+++ .github/copyai/copyaiv2.py
@@ -70,7 +70,8 @@ while True:
             sys.stdout.write('\r' + "Status: " + status + "...")
             sys.stdout.flush()
             if (status == "FAILED" or x == False):
-                break
+                print("There was an issue obtaining a response from Copy.AI")
+                exit(1)
 
         # Wait for the status to change to 'COMPLETE' and print the output
         if track_response_dict['data']['status'] == 'COMPLETE':
